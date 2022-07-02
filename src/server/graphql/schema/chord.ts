1a2c8eb198fa7bf38f38c3a540205258bc51cfc8
Сергей Тихомиров <tixomirov95@yandex.ru>
2022-07-03 01:22:54 +0300

    Добавил граф кл

---
+++ src/server/graphql/schema/chord.ts
@@ -0,0 +1,27 @@
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLList } from 'graphql'
+import { iChord, iGuitarString } from 'types/chord'
+
+export const GuitarStringType = new GraphQLObjectType<iGuitarString>({
+  name: "GuitarString", 
+  description: "GuitarString",
+  fields: {
+    index: { type: GraphQLInt },
+    fret: { type: GraphQLString },
+  }
+})
+
+export const ChordType = new GraphQLObjectType<iChord>({
+    name: "Chord",
+    description: "Аккорд",
+    fields: () => ({
+      instrument: { type: new GraphQLNonNull(GraphQLString) },
+      note: { type: new GraphQLNonNull(GraphQLString) },
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      startFret: { type: GraphQLInt },
+      barre: { type: GraphQLBoolean },
+      guitarStrings: {
+        type: new GraphQLList(GuitarStringType)
+      },
+      searchName: { type: new GraphQLNonNull(GraphQLString) }
+    })
+  })
